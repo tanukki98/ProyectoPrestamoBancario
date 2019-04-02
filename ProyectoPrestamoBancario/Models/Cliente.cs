d29c6ce88f36b157b97591ed41cba82904cfa690
Gonzalo Huamani <deku0298@hotmail.com>
2019-04-02 12:06:29 -0500

    Primeros cambios Pagina principal basica

---
+++ ProyectoPrestamoBancario/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoPrestamoBancario.Models
+{
+    public class Cliente
+    {
+        int idCliente { get; set; }
+        String apellidos { get; set; }
+        String nombres { get; set; }
+        
+    }
+}
